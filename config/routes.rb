9fc654ed3eb3a38766aff67250f4725b5f007c45
Jasontbk <jasontbk@jasontbk-VirtualBox.(none)>
2013-09-14 15:12:06 +0800

    remove index redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 MovieApp::Application.routes.draw do
  root 'main#index'
 
- get 'index', :to => 'main#index'
  get 'redirect', :to => 'main#redirect'
  get 'login' , :to => 'main#login'
  get 'logout' , :to => "main#logout"
