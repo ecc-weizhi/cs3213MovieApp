c85b6de47f2ff990426d8c9fa5aa8309eb8b7671
Jasontbk <jasontbk@jasontbk-VirtualBox.(none)>
2013-09-14 14:05:29 +0800

    added oauth2 gem, signin redirect and logout. also added new navbar based on twitter bootstrap

--- Gemfile
+++ Gemfile
@@ -24,4 +24,5 @@ group :production do
   gem 'rails_12factor', '0.0.2'
 end
 
+gem 'oauth2'
 gem 'backbone-on-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 MovieApp::Application.routes.draw do
  root 'main#index'
+
+ get 'index', :to => 'main#index'
+ get 'redirect', :to => 'main#redirect'
+ get 'login' , :to => 'main#login'
+ get 'logout' , :to => "main#logout"
 end
