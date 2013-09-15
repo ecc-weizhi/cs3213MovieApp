7b3eff887b97e8038b905984b597c8daf279099d
dvin89 <darren080589@gmail.com>
2013-09-15 20:32:28 +0800

    darrev_gon

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -16,7 +16,7 @@ class MainController < ApplicationController
 		:site => 'http://cs3213.herokuapp.com', 
 		:authorize_url => '/oauth/new')
 
-    @redirect_url = "http://localhost:3000/redirect"
+    @redirect_url = "http://super-movie-app.herokuapp.com/redirect"
     #@redirect_url = "http://super-movie-app.herokuapp.com//redirect"
   end
 
