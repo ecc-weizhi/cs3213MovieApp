f6985f660cd276cbee65c3a466c4a9299d4e4d83
dvin89 <darren080589@gmail.com>
2013-09-16 23:10:37 +0800

    darren_latest

--- app/assets/javascripts/views/movieCreate_View.js
+++ app/assets/javascripts/views/movieCreate_View.js
@@ -22,10 +22,10 @@ MovieApp.Views.MovieCreateView = Backbone.View.extend({
     renderString += "<td colspan=2><h1>Create new movie</h1></td></tr>";
     renderString += "<tr><td>Title: </td><td><input type='text' name='movie[title]' id='movie_title'></td></tr>";
     renderString += "<tr><td>Summary: </td><td> <input type='text' name='movie[summary]' id='summary'></td></tr>";
-    renderString += "<tr><td>Image: </td><td> <input type='file' name='movie[img]' id='movie_img'></td>/tr>";
+    renderString += "<tr><td>Image: </td><td> <input type='file' name='movie[img]' id='movie_img'></td></tr>";
     renderString += "<tr><td colpsan=2  style='text-align: center;'><button class='btn btn-primary' id='Create' type='button'>Create</button></td></tr></table></form></div>";
-    $(this.el).append(renderString);
     
+    $(this.el).append(renderString);    
 
     return this;
   },
