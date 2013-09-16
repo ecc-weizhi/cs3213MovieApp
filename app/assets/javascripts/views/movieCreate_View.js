c85baf1885e8682661aff91081644d5e3e6c18fe
ecc-weizhi <ecc.weizhi@gmail.com>
2013-09-16 13:46:23 +0800

    add working create movie

--- app/assets/javascripts/views/movieCreate_View.js
+++ app/assets/javascripts/views/movieCreate_View.js
@@ -18,85 +18,40 @@ MovieApp.Views.MovieCreateView = Backbone.View.extend({
     $(this.el).html(myNavBarView.render().el);
 
     $(this.el).append("<h1>Create new movie</h1>");
-    //$(this.el).append("<div id='movieForm'>");
-    $(this.el).append("<form name='newMovieForm' method='post' data-remote='true' enctype='multipart/form-data'>");
-    $(this.el).append("Title: <input type='text' id='titleInput'><br>");
-    $(this.el).append("Summary: <input type='text' id='summaryInput'><br>");
-    $(this.el).append("img: <input type='file' id='imgInput'><br>");
-    $(this.el).append("<button class='Create' type='button'>Create</button>");
-    //$(this.el).append("</form>");
-    $(this.el).append("</div>");
+    $(this.el).append("<div id='movieForm'><form name='movie' method='POST'>Title: <input type='text' name='movie[title]' id='movie_title'><br>Summary: <input type='text' name='movie[summary]' id='summary'><br>img: <input type='file' name='movie[img]' id='movie_img'><br><button class='Create' type='button'>Create</button></form></div>");
 
     return this;
   },
 
-  submit_movie_info: function(){
-  	alert("btn clicked");
-  	var file = document.getElementById('imgInput').files[0];
-  	console.log(file);
-  	
-  	var reader = new FileReader();
-   
-    reader.onload = (function (){
-    	alert("done");
-    	console.log(reader.result);
-    	var obj = {};
-			obj["title"] = document.getElementById("titleInput").value;
-			obj["summary"] = document.getElementById("summaryInput").value;
-			obj["img"] = reader.result;
-			obj["access_token"] = gon.token;
-			
-			console.log(obj);
-			var NewMovie = Backbone.Model.extend({url: "http://cs3213.herokuapp.com/movies.json"});
-			var myNewMovie = new NewMovie();
-    console.log("here?");
-    	myNewMovie.set(obj);
-    	
-    	console.log(myNewMovie);
-    	
-    	myNewMovie.save({success: function(){ alert("win"); } });
-			
+  submit_movie_info: function(e){
+  	$(e.target).parent().ajaxSubmit({
+      url:  "http://cs3213.herokuapp.com/movies.json",
+      dataType: 'json',
+      data: {"access_token": gon.token},
+      method: "POST",
+      error: function(error, err) {
+	      alert("Please log in."); 
+      },
+      success: function(msg) {
+   	    window.router.navigate("movie/" + msg.id, {trigger: true});
+      },
+      beforeSubmit: function(arr, $form, options) { 
+       	if($("#movie_title").val() == "") {
+          alert("Please enter movie title");
+          return false;
+        } 
+        else if($("#movie_img").val() == "")
+        {
+       	 	alert("Please choose image.");
+          return false;
+        }
+        else 
+        {
+          return true;
+        }
+  		}
     });
-    
-    
-    
-    /*(function(reader) {
-        return function(e) {
-            alert("done");
-            var obj = {};
-    				obj["title"] = document.getElementById("titleInput").value;
-    				obj["summary"] = document.getElementById("summaryInput").value;
-    				obj["img"] = ;
-        };
-    })(reader);*/
-    reader.readAsDataURL(file);
-    
-    
-  	/*
-  	var formData = new FormData();
-  	formData.append('file', file);
-  	
-  	console.log(formData);
-  	*/
-  	/*
-    var reader = new FileReader();
-  	
-    var obj = {};
-    obj["title"] = document.getElementById("titleInput").value;
-    obj["summary"] = document.getElementById("summaryInput").value;
-    obj["img"] = document.getElementById("imgInput");
-    
-    var NewMovie = Backbone.Model.extend({
-      url: "http://cs3213.herokuapp.com/movies.json"
-    });
-    var myNewMovie = new NewMovie;
-    
-    myNewMovie.set(obj);
-    console.log(myNewMovie);
-    */
-    //myNewMovie.save(obj,  {success: function(){ alert("win"); } });
-
-    //window.router.navigate("create", {trigger: true});
+    return false;
   },
 
   create_new_movie : function() {
